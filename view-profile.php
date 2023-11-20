47823a15223b51a1d12a67e63a6ac8a1e44ba65e
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-20 12:51:20 -0600

    Create view-profile.php

---
+++ view-profile.php
@@ -0,0 +1,29 @@
+<h1>My Profile</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Email</th>
+      <th>Password</th>  
+      <th></th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($profile = $profile->fetch_assoc()){
+?>
+  <tr>
+   <td><?php echo $profile['user_id'];?></td> 
+   <td><?php echo $profile['user_name'];?></td>
+   <td><?php echo $profile['user_email'];?></td>
+   <td><?php echo $profile['user_password'];?></td>
+
+  </tr>
+<?php
+}  
+?>
+    </tbody>
+  </table>
+</div>
