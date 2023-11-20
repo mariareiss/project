9a6bb884b0f794120cbdb8d47e2a07a5fd7eb04a
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-20 13:19:40 -0600

    Update model-profile.php

--- model-profile.php
+++ model-profile.php
@@ -12,4 +12,43 @@ function selectProfile() {
         throw $e;
     }
 }
+function insertProfile($uName, $uEmail, $uPassword) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `User` (`user_name`, `user_email`, `user_password`) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $uName, $uEmail, $uPassword);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateProfile($uName, $uEmail, $uPassword, $uid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `User` set `user_name`= ?, `user_email`= ?, `user_password`= ? where user_id = ?");
+        $stmt->bind_param("sssi", $uName, $uEmail, $uPassword, $uid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteProfile($uid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from `User` where customer_id=?");
+        $stmt->bind_param("i", $uid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
