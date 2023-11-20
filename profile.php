e366f821f8074d1ffd73970bb3dabeca261c7ea1
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-20 14:48:41 -0600

    Update profile.php

--- profile.php
+++ profile.php
@@ -7,21 +7,21 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']){
     case "Add":
-    if (insertUser($_POST['uName'], $_POST['uEmail'], $_POST['uPassword'])){
+    if (insertProfile($_POST['uName'], $_POST['uEmail'], $_POST['uPassword'])){
       echo '<div class="alert alert-success" role="alert">User added.</div>';
     } else {
       echo '<div class="alert alert-error" role="alert">Error</div>';      
     }
     break;
     case "Edit":
-    if (updateUser($_POST['uName'], $_POST['uEmail'], $_POST['uPassword'], $_POST['uid'])){
+    if (updateProfile($_POST['uName'], $_POST['uEmail'], $_POST['uPassword'], $_POST['uid'])){
       echo '<div class="alert alert-success" role="alert">User edited.</div>';
     } else {
       echo '<div class="alert alert-error" role="alert">Error</div>';      
     }
     break;
     case "Delete":
-    if (deleteUser($_POST['uid'])){
+    if (deleteProfile($_POST['uid'])){
       echo '<div class="alert alert-success" role="alert">User deleted.</div>';
     } else {
       echo '<div class="alert alert-error" role="alert">Error</div>';      
