899dda988c97b0e27f973cfda003ffb20b22eb79
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-20 12:47:53 -0600

    Create model-profile.php

---
+++ model-profile.php
@@ -0,0 +1,15 @@
+<?php
+function selectProfile() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT user_id, user_name, user_email, user_password FROM `User`");        
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
