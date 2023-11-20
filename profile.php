6f0108c866c6019af6155df9a5f049911d0cb9a0
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-20 13:07:21 -0600

    Update profile.php

--- profile.php
+++ profile.php
@@ -2,9 +2,34 @@
 require_once("util-db.php");
 require_once("model-profile.php");
 
-$pageTitle = "My Profile";
+$pageTitle = "Profiles";
 include "view-header.php";
-$profile = selectProfile();
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']){
+    case "Add":
+    if (insertUser($_POST['uName'], $_POST['uEmail'], $_POST['uPassword'])){
+      echo '<div class="alert alert-success" role="alert">User added.</div>';
+    } else {
+      echo '<div class="alert alert-error" role="alert">Error</div>';      
+    }
+    break;
+    case "Edit":
+    if (updateUser($_POST['uName'], $_POST['uEmail'], $_POST['uPassword'], $_POST['uid'])){
+      echo '<div class="alert alert-success" role="alert">User edited.</div>';
+    } else {
+      echo '<div class="alert alert-error" role="alert">Error</div>';      
+    }
+    break;
+    case "Delete":
+    if (deleteUser($_POST['uid'])){
+      echo '<div class="alert alert-success" role="alert">User deleted.</div>';
+    } else {
+      echo '<div class="alert alert-error" role="alert">Error</div>';      
+    }
+    break;
+  }
+}
+$customers = selectProfile();
 include "view-profile.php";
 include "view-footer.php";
 ?>
