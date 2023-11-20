9e523554e45aee151f78943edccf071cdc03ab14
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-20 12:53:25 -0600

    Update model-profile.php

--- model-profile.php
+++ model-profile.php
@@ -1,5 +1,5 @@
 <?php
-function selectProfile() {
+function selectProfiles() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT user_id, user_name, user_email, user_password FROM `User`");        
